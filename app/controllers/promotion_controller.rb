79a0d43ee18a2051b92de4b849d16ec815def41e
Rafael Mateus <rafaelmateus@hotmail.com.br>
2018-11-22 20:11:15 +0000

    fix

--- app/controllers/promotion_controller.rb
+++ app/controllers/promotion_controller.rb
@@ -1,5 +1,5 @@
 class PromotionController < ApplicationController
   def index
-    @drugstore_medicines = DrugstoreMedicine.where('drugstore_medicines.promotion_price IS NOT NULL')
+    @drugstore_medicines = DrugstoreMedicine.where.not(promotion_price: nil)
   end
 end
