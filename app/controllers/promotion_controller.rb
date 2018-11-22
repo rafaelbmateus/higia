96ff892b99f0af27668c02767b208e4200920e76
Rafael Mateus <rafaelmateus@hotmail.com.br>
2018-11-22 16:14:41 +0000

    test promotion price

--- app/controllers/promotion_controller.rb
+++ app/controllers/promotion_controller.rb
@@ -1,5 +1,5 @@
 class PromotionController < ApplicationController
   def index
-    @drugstore_medicines = DrugstoreMedicine.where.not(promotion_price: nil)
+    @drugstore_medicines = DrugstoreMedicine.all #where.not(promotion_price: nil)
   end
 end
